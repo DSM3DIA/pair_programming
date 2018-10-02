a6249155a0948ceb093c04c250bfdf47201ed4f4
DSM3DIA <DSM3DIA@users.noreply.github.com>
2018-10-02 07:52:06 -0400

    Moved instructions to Readme

--- src/index.js
+++ src/index.js
@@ -14,11 +14,6 @@ function App() {
   return (
     <div className="App">
       <h1>Colorful Buttons</h1>
-      Loop through the "colors" array and return a button for each color.
-      <ol>
-        <li>Set the button's background color to color.value</li>
-        <li>Set the button's value to color.color</li>
-      </ol>
       {
         // Buttons go here
       }
