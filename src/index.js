9a4f13ce7f32d60525eba239376641a487350d09
DSM3DIA <DSM3DIA@users.noreply.github.com>
2018-10-02 07:48:03 -0400

    Added instructions

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+import colors from "./colors.json";
+
+/**
+ * React - Inline Styles
+ * https://react-cn.github.io/react/tips/inline-styles.html
+ */
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Colorful Buttons</h1>
+      Loop through the "colors" array and return a button for each color.
+      <ol>
+        <li>Set the button's background color to color.value</li>
+        <li>Set the button's value to color.color</li>
+      </ol>
+      {
+        // Buttons go here
+      }
     </div>
   );
 }
